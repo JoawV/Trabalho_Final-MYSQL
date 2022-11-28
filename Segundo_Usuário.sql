5a68b7a5fce183fcab5cd87ded82ccbbbfa05744
João Vítor <112258047+JoawV@users.noreply.github.com>
2022-11-28 20:08:02 -0300

    Create Segundo_Usuário.sql

---
+++ Segundo_Usuário.sql
@@ -0,0 +1,19 @@
+use exercicios;
+
+select * from atual_campeão;
+select * from maior_campeão;
+select * from nunca_ganhou;
+select * from vice_campeão;
+select * from ultimo_vice_campeão;
+
+insert into atual_campeão values
+('25', 'Juventude', '25','25','25','25','25','25');
+
+/*Não funciona*/
+select * from TIMES;
+select * from JOGADORES;
+select * from ESTADIOS;
+select * from CIDADES;
+select * from PAIS;
+select * from GOLS;
+select * from TITULOS;
