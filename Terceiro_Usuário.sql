6de2fbd74b78f2da911f7e4d75a0f6546e85e744
João Vítor <112258047+JoawV@users.noreply.github.com>
2022-11-28 20:09:00 -0300

    Create Terceiro_Usuário.sql

---
+++ Terceiro_Usuário.sql
@@ -0,0 +1,31 @@
+use Exercicios;
+
+insert into times values
+('22', 'Juventude', '22','22','22','22','22','22');
+
+insert into jogadores values
+('17', 'Maradona', '17','17');
+
+insert into estadios values
+('17', 'Estádio do Vale', '20.000','17','17');
+
+insert into cidades values
+('17', 'Gramado', '17');
+
+insert into pais values
+('17', 'Itália');
+
+insert into gols values
+('17', '28', '17');
+
+insert into titulos values
+('17', '15', '17');
+
+/*Não funciona*/
+select * from TIMES;
+select * from JOGADORES;
+select * from ESTADIOS;
+select * from CIDADES;
+select * from PAIS;
+select * from GOLS;
+select * from TITULOS;# Trabalho_Final-MYSQL
