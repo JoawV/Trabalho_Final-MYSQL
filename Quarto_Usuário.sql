334305f1e22fce997cafc671188f487f78bbcde6
João Vítor <112258047+JoawV@users.noreply.github.com>
2022-11-28 20:13:12 -0300

    Create Quarto_Usuário.sql

---
+++ Quarto_Usuário.sql
@@ -0,0 +1,13 @@
+use exercicios;
+
+select * from TIMES;
+select * from JOGADORES;
+select * from ESTADIOS;
+select * from CIDADES;
+select * from PAIS;
+select * from GOLS;
+select * from TITULOS;
+
+/*Não funciona*/
+insert into jogadores values
+('18', 'Kvaratskhelia', '18', '18');
